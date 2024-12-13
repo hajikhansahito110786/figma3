131a186a0253b052438984bf58403e91c060cd0f
hajikhansahito110786 <hajikhansahito110786@gmail.com>
2024-12-13 11:49:45 +0500

    Add files via upload

---
+++ src/app/Startupfigma.tsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+
+import Navbarnew from './componentsnew/navbarnew'
+import Herosection from './componentsnew/herosection'
+import Clogo from './componentsnew/clogo'
+import Feutureproduct from './componentsnew/Feutureproduct'
+
+const Startupfigma = () => {
+  return (
+    <div>
+
+     
+
+<Navbarnew/>
+<Herosection/>
+<Clogo/>
+<Feutureproduct/> 
+    </div>
+   
+  )
+}
+
+export default Startupfigma;
---
+++ src/app/page.tsx
@@ -0,0 +1,13 @@
+//import Image from "next/image";
+
+
+import Startupfigma from "./Startupfigma";
+
+
+export default function Home() {
+  return (
+    <div >
+      <Startupfigma/>
+    </div>
+  );
+}
